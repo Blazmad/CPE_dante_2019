7acc16a276918bd2c0fe173a8f04074edf8d52f9
thomas grand <tgrand@localhost.localdomain>
2020-03-11 15:07:31 +0100

    add error gestion and merge

--- generator/src/check_argument.c
+++ generator/src/check_argument.c
@@ -10,10 +10,10 @@
 int check_argument(char *arg1, char *arg2)
 {
     for (int i = 0; arg1[i]; i++)
-        if (arg1[i] < '0' || arg1[i] > '9')
+        if (arg1[i] < '1' || arg1[i] > '9')
             return (84);
     for (int i = 0; arg2[i]; i++)
-        if (arg2[i] < '0' || arg2[i] > '9')
+        if (arg1[i] < '1' || arg1[i] > '9')
             return (84);
     return (0);
 }

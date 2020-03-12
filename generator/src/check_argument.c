d396b2c89a9ae7ed287b8e2ba5a5d497134c69fe
Clement Madzar <cmadzar@localhost.localdomain>
2020-03-12 10:21:17 +0100

    check arg if 1 1

--- generator/src/check_argument.c
+++ generator/src/check_argument.c
@@ -11,6 +11,11 @@ int check_argument(char *arg1, char *arg2)
 {
     if (arg1[0] == '0' || arg2[0] == '0')
         return (84);
+    if (arg1[0] == '1' && arg1[0] == '1' &&
+        (arg1[1] == '\0' && arg1[1] == '\0')) {
+        my_putstr("*");
+        exit(0);
+    }
     for (int i = 0; arg1[i]; i++)
         if (arg1[i] < '0' || arg1[i] > '9')
             return (84);
